bfcccf9631bddca817a7135232ab7db04bbf575d
Ruddy Monlouis <r.monlouis@gmail.com>
2022-01-31 22:01:06 +0100

    Add DAO

---
+++ src/main/java/com/example/projetappel/dao/EnseignantDao.java
@@ -0,0 +1,27 @@
+package com.example.projetappel.dao;
+
+import com.example.projetappel.model.Enseignant;
+
+public class EnseignantDao extends DAO<Enseignant> {
+
+    @Override
+    public Enseignant find(long id) {
+        return null;
+    }
+
+    @Override
+    public Enseignant create(Enseignant obj) {
+        return null;
+    }
+
+    @Override
+    public Enseignant update(Enseignant obj) {
+        return null;
+    }
+
+    @Override
+    public void delete(Enseignant obj) {
+
+    }
+
+}
